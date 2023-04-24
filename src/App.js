0b35a1f276814e91c4836a9a5da710248ad71e67
Marcelo Meireles <macelopimeireles@gmail.com>
2023-04-24 01:07:12 +0100

    feat: skills with radio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import data from './assets/character_sheet.json';
 
 const RACES = [
-  {id:0, name: 'Donta'}
+  { id: 0, name: 'Donta' }
 ];
 
 const App = () => {
@@ -11,49 +11,59 @@ const App = () => {
   const titlesSkills = Object.keys(data[titles[2]]);
 
   const renderTitles = () => titlesInfo.map(item =>
-    (<li key={item} className="p-4 pt-2 pb-2">
-      <div className='inline-flex w-1/4'>{item}</div>
-      <div className='inline-flex w-3/4'><input className='w-3/4'/></div>
-    </li>)
+  (<li key={item} className="p-4 pt-2 pb-2">
+    <div className='inline-flex w-1/4'>{item}</div>
+    <div className='inline-flex w-3/4'><input className='w-3/4' /></div>
+  </li>)
   );
 
   const renderAttributes = () => titlesAttributes.map(item =>
-    (<li key={item} className="p-4 pt-0 pb-0">
-      <div key={item} className='inline-flex w-3/12'>{item}</div>
-      <div className='inline-flex w-1/12'><input className='w-full m-2'/></div>
-      <div className='inline-flex w-1/12'><input className='w-full m-2'/></div>
-      <div className='inline-flex w-1/12'><input className='w-full m-2'/></div>
-      <div className='inline-flex w-1/12'><input className='w-full m-2'/></div>
-      <div className='inline-flex w-1/12'><input className='w-full m-2'/></div>
-      <a href="#" className='px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-300 transform bg-gray-400 rounded-md dark:bg-gray-700 hover:bg-primary dark:hover:bg-primary'>
-        Roll
-      </a>
-    </li>)
+  (<li key={item} className="p-4 pt-0 pb-0">
+    <div key={item} className='inline-flex w-3/12'>{item}</div>
+    <div className='inline-flex w-1/12'><input className='w-full m-2' /></div>
+    <div className='inline-flex w-1/12'><input className='w-full m-2' /></div>
+    <div className='inline-flex w-1/12'><input className='w-full m-2' /></div>
+    <div className='inline-flex w-1/12'><input className='w-full m-2' /></div>
+    <div className='inline-flex w-1/12'><input className='w-full m-2' /></div>
+    <a href="#" className='px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-300 transform bg-gray-400 rounded-md dark:bg-gray-700 hover:bg-primary dark:hover:bg-primary'>
+      Roll
+    </a>
+  </li>)
   );
-  
+
   const renderSkillList = (skillList, title) => skillList.map(item =>
-    (<li key={item} className="p-8 pt-0 pb-0">
-      <div key={item} className='inline-flex w-1/6'>{item}</div>
-      <div key={item} className='inline-flex w-1/6'>({ data.Skills[title][item].Attribute })</div>
-      <div className='inline-flex w-1/12'><input className='w-full m-2'/></div>
-      <div className='inline-flex w-1/12'><input className='w-full m-2'/></div>
-      <div className='inline-flex w-1/12'><input className='w-full m-2'/></div>
-      <div className='inline-flex w-1/12'><input className='w-full m-2'/></div>
-      <div className='inline-flex w-1/12'><input className='w-full m-2'/></div>
-      <div className='inline-flex w-1/12'><input className='w-full m-2'/></div>
-      <a href="#" className='px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-300 transform bg-gray-400 rounded-md dark:bg-gray-700 hover:bg-primary dark:hover:bg-primary'>
-        Roll
-      </a>
-    </li>)
+  (<li key={item} className="p-8 pt-0 pb-0">
+    <div key={item} className='inline-flex w-1/6 font-bold'>{item}</div>
+    <div key={item} className='inline-flex w-1/6'>({data.Skills[title][item].Attribute})</div>
+    <div className='inline-flex w-1/12'><input type='radio' name={item+'level'} className='w-full' /></div>
+    <div className='inline-flex w-1/12'><input type='radio' name={item+'level'} className='w-full' /></div>
+    <div className='inline-flex w-1/12'><input type='radio' name={item+'level'} className='w-full' /></div>
+    <div className='inline-flex w-1/12'><input type='radio' name={item+'level'} className='w-full' /></div>
+    <div className='inline-flex w-1/12'><input type='radio' name={item+'level'} className='w-full m-2' /></div>
+    <div className='inline-flex w-1/12'><input className='w-full m-2' /></div>
+    <a href="#" className='px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-300 transform bg-gray-400 rounded-md dark:bg-gray-700 hover:bg-primary dark:hover:bg-primary'>
+      Roll
+    </a>
+  </li>)
   );
 
   const renderSkills = () => titlesSkills.map(item =>
-    (<li key={item}>
-      <div className="rounded-md bg-gray-100 dark:bg-gray-800 pt-4 m-2 overflow-hidden">
-              <h1 className="px-3 py-2 mx-3 mb-2 font-bold text-gray-400 dark:bg-gray-700 dark:text-gray-200 tracking-wider bg-gray-200 rounded-md">{item}</h1>
-      </div>
-      {renderSkillList(Object.keys(data.Skills[item]), item)}
-    </li>)
+  (<li key={item}>
+    <div className="rounded-md bg-gray-100 dark:bg-gray-800 pt-4 m-2 overflow-hidden">
+      <h1 className="px-3 py-2 mx-3 mb-2 font-bold text-gray-400 dark:bg-gray-700 dark:text-gray-200 tracking-wider bg-gray-200 rounded-md">{item}</h1>
+    </div>
+    <li className="p-8 pt-0 pb-0 text-gray-300">
+                  <div className='inline-flex w-1/6'>Skill</div>
+                  <div className='inline-flex w-1/6'>(Attribute)</div>
+                  <div className='inline-flex w-1/12'>Untr.</div>
+                  <div className='inline-flex w-1/12'>Basic</div>
+                  <div className='inline-flex w-1/12'>Trained</div>
+                  <div className='inline-flex w-1/12'>Spec</div>
+                  <div className='inline-flex w-1/12'>Master</div>
+                  <div className='inline-flex w-1/12'>Final</div>
+    </li>
+    {renderSkillList(Object.keys(data.Skills[item]), item)}
+  </li>)
   );
 
   return (
@@ -89,7 +99,10 @@ const App = () => {
           <div className="w-2/6">
             <div className="rounded-md bg-gray-100 dark:bg-gray-800 pt-4 m-2 overflow-hidden">
               <h1 className="px-3 py-2 mx-3 mb-2 font-bold text-gray-800 dark:bg-gray-700 dark:text-gray-200 tracking-wider bg-gray-200 rounded-md">{titles[2]}</h1>
-              <ul>{renderSkills()}</ul>
+
+              <ul>
+                {renderSkills()}
+              </ul>
             </div>
           </div>
 
