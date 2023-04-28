2f774f79501405f54ceedd331eedc9efc47640f6
Marcelo Meireles <macelopimeireles@gmail.com>
2023-04-28 22:57:40 +0100

    feat: form Info

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,35 +1,67 @@
-import React from 'react'
-import data from '../assets/mocks/character_sheet.json';
-import { CONSTANTS } from '../assets/mocks/const.js';
+import React, { useEffect, useState } from "react";
+import data from "../assets/mocks/character_sheet.json";
+import { CONSTANTS } from "../assets/mocks/const.js";
+
+import { Input, Select, Option } from "@material-tailwind/react";
 
 function Info() {
     const titles = Object.keys(data);
     const titlesInfo = Object.keys(data[titles[0]]);
+    const titleCss = "px-3 py-2 mx-3 mb-2 font-bold text-blue-800 tracking-wider bg-blue-200 rounded-md";
 
-    const renderOptions = (item) => {
-        const iterator = (item + 's').toString().toUpperCase();
-        const response = CONSTANTS[iterator].map(option => <option key={option}>{option}</option>);
+    // Adicione um estado para armazenar os valores do formulário
+    const [formState, setFormState] = useState({
+        name: '',
+        player: '',
+        race: '',
+        nation: '',
+        background: '',
+        size: ''
+    });
+
+    const renderOptions = (item, index) => {
+        const iterator = (item + "s").toString().toUpperCase();
+        const response = CONSTANTS[iterator].map(option => <Option key={option} value={item+','+option}>{option}</Option>);
         return response;
     }
 
+    const handleChange = (event) => {
+        // Atualize o estado com o novo valor do formulário
+        if (typeof event === 'string' || event instanceof String) {
+            let values = event.split(',');
+            setFormState({
+                ...formState,
+                [values[0].toLowerCase()]: values[1]
+            });
+        } else {
+            setFormState({
+                ...formState,
+                [(event.target.name).toLowerCase()]: event.target.value
+            });
+        }
+    };
+
+    useEffect(()=> console.log(formState),[formState])
+
     const renderTitles = () => titlesInfo.map((item, index) =>
     (<li key={item} className="p-4 pt-2 pb-2">
-        <div className='inline-flex w-1/4'>{item}</div>
-        {
-            (index > 1)
-                ? <div className='inline-flex w-3/4'><select className='w-full'>
-                        <option></option>
-                        { renderOptions(item) }
-                    </select></div>
-                : <div className='inline-flex w-3/4'><input className='w-full' /></div>
+        {(index > 1)
+            ? (<Select name={item} label={item} onChange={handleChange}>
+                {renderOptions(item, index)}
+            </Select>)
+            : <Input name={item} label={item} onChange={handleChange} value={formState[item]} />
         }
     </li>)
     );
 
     return (
         <div className="rounded-md bg-blue-100 py-4">
-            <h1 className="px-3 py-2 mx-3 mb-2 font-bold text-blue-800 tracking-wider bg-blue-200 rounded-md">{titles[0]}</h1>
-            <ul className='columns-1 md:columns-2 gap-0'>{renderTitles()}</ul>
+            <h1 className={titleCss}>{titles[0]}</h1>
+            <form>
+                <ul className="columns-1 md:columns-2 gap-0">
+                    {renderTitles()}
+                </ul>
+            </form>
         </div>
     )
 }
