108e4010f6b8f1a29a77b342a4460d0889ca2188
Marcelo Meireles <macelopimeireles@gmail.com>
2023-04-28 22:44:13 +0100

    feat: new weapons + equip components

--- src/App.js
+++ src/App.js
@@ -8,52 +8,50 @@ import Portrait from './components/Portrait';
 import Stats from './components/Stats';
 import Magic from './components/Magic';
 import Traits from './components/Traits';
+import Weapons from './components/Weapons';
+import Equipments from './components/Equipments';
 
 const App = () => {
+  const rightColCss =    "grid col-span-6 row-span-1 lg:col-span-2 lg:row-span-3";
+  const rowCss =         "grid grid-flow-row lg:grid-flow-col gap-4";
+  const downSectionCss = "grid grid-flow-row lg:grid-col-6 lg:grid-flow-col gap-4 p-4 w-full";
+  const upSectionCss = downSectionCss + " lg:grid-row-3";
+
   return (
     <div className="App tracking-wide font-roboto min-h-screen grid bg-gray-900">
       <Header />
-
       <section>
-
-        <div className="grid grid-flow-row lg:grid-col-6 lg:grid-row-3 lg:grid-flow-col gap-4 p-4 w-full">
+        <div className={upSectionCss}>
           <div className="col-span-6">
-            <div className="grid grid-flow-row lg:grid-flow-col gap-4">
+            <div className={rowCss}>
               <div className="col-span-1 xl:w-1/2"><Logo /></div>
               <div className="col-span-5"><Info /></div>
             </div>
           </div>
 
           <div className="col-span-6">
-            <div className="grid grid-flow-row lg:grid-flow-col gap-4">
+            <div className={rowCss}>
               <Attributes />
               <Stats />
             </div>
           </div>
 
           <div className="col-span-6">
-            <div className="grid grid-flow-row lg:grid-flow-col gap-4">
+            <div className={rowCss}>
               <Traits />
               <Magic />
             </div>
           </div>
 
-          <div className="grid col-span-6 row-span-1 lg:col-span-2 lg:row-span-3"><Portrait /></div>
+          <div className={rightColCss}><Portrait /></div>
         </div>
 
-        <div className="grid grid-flow-row lg:grid-col-6 lg:grid-flow-col gap-4 p-4 w-full">
-
-            <Skills />
+        <div className={downSectionCss}>
+          <Skills />
           
           <div className="grid grid-flow-row lg:grid-col-6 gap-4 w-full">
-            <div className="rounded-md bg-blue-100 py-4 overflow-hidden">
-              <h1 className="px-3 py-2 mx-3 mb-4 font-bold text-blue-800 tracking-wider bg-blue-200 rounded-md">Weapons</h1>
-            </div>
-
-            <div className="rounded-md bg-blue-100 py-4 overflow-hidden">
-              <h1 className="px-3 py-2 mx-3 mb-4 font-bold text-blue-800  tracking-wider bg-blue-200 rounded-md">Equipments</h1>
-            </div>
-
+            <Weapons />
+            <Equipments />
           </div>
         </div>
 
---
+++ src/components/Equipments.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Equipments() {
+  return (
+    <div className="rounded-md bg-blue-100 py-4 overflow-hidden">
+        <h1 className="px-3 py-2 mx-3 mb-4 font-bold text-blue-800  tracking-wider bg-blue-200 rounded-md">Equipments</h1>
+    </div>
+  )
+}
+
+export default Equipments
