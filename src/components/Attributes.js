b88b19187c0db27a07006265c3bd04bbc3f8f439
Marcelo Meireles <macelopimeireles@gmail.com>
2023-04-29 22:12:29 +0100

    fix: attributes from grid

--- src/components/Attributes.js
+++ src/components/Attributes.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Button } from "@material-tailwind/react";
-// import data from '../assets/mocks/character_sheet.json';
+// import data from "../assets/mocks/character_sheet.json";
 
 const Attributes = () => {
   const initValues = {
@@ -73,25 +73,25 @@ const Attributes = () => {
 
   const renderAttributes = () => titlesAttributes.map(item =>
   (<li key={item} className="px-4">
-    <div key={item} className='inline-flex w-1/12'>
-      <span className='font-bold'>{item.substring(0, 3).toUpperCase()}</span>
+    <div key={item} className="inline-flex w-1/6 md:w-1/12">
+      <span className="font-bold">{item.substring(0, 3).toUpperCase()}</span>
     </div>
-    <div key={item} className='hidden md:inline-flex md:w-3/12 text-center'>
-      <span className='hidden md:block'>{item}</span>
+    <div key={item} className="hidden w-1/6 md:inline-flex md:w-3/12 text-center">
+      <span className="hidden md:block">{item}</span>
     </div>
-    <div className='inline-flex w-1/12 text-center'>{formState[item].base}</div>
-    <div className='inline-flex w-1/6'>
-      <input type="number" alt={item} onChange={handleExtraChange} value={formState[item].extras} className='w-full m-2 text-center' />
+    <div className="inline-flex w-1/12 text-center">{formState[item].base}</div>
+    <div className="inline-flex w-1/6">
+      <input type="number" alt={item} onChange={handleExtraChange} value={formState[item].extras} className="w-full m-2 text-center" />
     </div>
-    <div className='inline-flex w-1/6'>
-      <input type="number" alt={item} onChange={handleEnhancementChange} value={formState[item].enhancement} className='w-full m-2 text-center' />
+    <div className="inline-flex w-1/6">
+      <input type="number" alt={item} onChange={handleEnhancementChange} value={formState[item].enhancement} className="w-full m-2 text-center" />
     </div>
-    <div className='inline-flex w-1/6 text-center'>
+    <div className="inline-flex w-1/6 text-center">
       <Button variant="outlined" size="sm">
         {formState[item].total}
       </Button>
     </div>
-    <div className='inline-flex w-1/12 text-center'>{formState[item].modifier}</div>
+    <div className="inline-flex w-1/12 text-left">{formState[item].modifier}</div>
   </li>)
   );
 
@@ -99,6 +99,14 @@ const Attributes = () => {
     <div className="rounded-md bg-blue-100 py-4 overflow-hidden">
       <h1 className="px-3 py-2 mx-3 mb-2 font-bold text-blue-800 tracking-wider bg-blue-200 rounded-md">Attributes</h1>
       <ul>
+        <li className="px-4">
+          <div className="inline-flex w-1/6 md:w-2/6 lg:w-2/6 text-blue-500 text-xs"></div>
+          <div className="inline-flex w-2/12 text-blue-500 text-xs text-center">Base</div>
+          <div className="inline-flex w-2/12 text-blue-500 text-xs text-center">Extr.</div>
+          <div className="inline-flex w-1/12 text-blue-500 text-xs">Enha.</div>
+          <div className="inline-flex w-2/12 text-blue-500 text-xs">Total</div>
+          <div className="inline-flex w-1/12 text-blue-500 text-xs">Mod.</div>
+        </li>
         {renderAttributes()}
       </ul>
     </div>
