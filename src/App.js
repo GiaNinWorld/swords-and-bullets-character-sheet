2bb0c75a4bee6ad467b146de93b83cfc36fdaf18
Marcelo Meireles <macelopimeireles@gmail.com>
2023-04-29 15:25:45 +0100

    fix: grid

--- src/App.js
+++ src/App.js
@@ -12,38 +12,34 @@ import Weapons from './components/Weapons';
 import Equipments from './components/Equipments';
 
 const App = () => {
-  const rightColCss =    "grid col-span-6 row-span-1 lg:col-span-2 lg:row-span-3";
-  const rowCss =         "grid grid-flow-row lg:grid-flow-col gap-4";
-  const downSectionCss = "grid grid-flow-row lg:grid-col-6 lg:grid-flow-col gap-4 p-4 w-full";
-  const upSectionCss = downSectionCss + " lg:grid-row-3";
+  const rightColCss =    "grid col-span-5 row-span-1 lg:col-span-3 lg:row-span-2";
+  const rowCss =         "grid grid-flow-row lg:grid-col-6 lg:grid-flow-col gap-4 col-span-6";
+  const downSectionCss = "grid grid-flow-row lg:grid-col-5 lg:grid-flow-col gap-4 p-4 w-full";
+  const upSectionCss =    downSectionCss + " lg:grid-row-3";
 
   return (
-    <div className="App tracking-wide font-roboto min-h-screen grid bg-gray-900">
+    <div className="font-roboto min-h-screen grid bg-gray-900">
       <Header />
       <section>
         <div className={upSectionCss}>
-          <div className="col-span-6">
-            <div className={rowCss}>
-              <div className="col-span-1 xl:w-1/2"><Logo /></div>
-              <div className="col-span-5"><Info /></div>
-            </div>
+          <div className={rowCss}>
+            <div className="col-span-1 max-w-sm"><Logo /></div>
+            <div className="col-span-5"><Info /></div>
           </div>
 
-          <div className="col-span-6">
-            <div className={rowCss}>
+          <div className={rowCss}>
               <Attributes />
               <Stats />
-            </div>
-          </div>
-
-          <div className="col-span-6">
-            <div className={rowCss}>
-              <Traits />
-              <Magic />
-            </div>
           </div>
 
           <div className={rightColCss}><Portrait /></div>
+        </div>
+
+        <div className="col-span-6">
+          <div className={rowCss}>
+            <Traits />
+            <Magic />
+          </div>
         </div>
 
         <div className={downSectionCss}>
