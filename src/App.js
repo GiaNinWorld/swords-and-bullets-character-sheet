8f19bf6db9d9ba6b1709dd95fbd4a7597e92e309
Marcelo Meireles <macelopimeireles@gmail.com>
2023-05-01 10:26:20 +0100

    feat: form context + attributes in context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
+import React, { useState } from "react";
+
+import FormContext from "./components/FormContext";
 import Header from './components/Header';
 import Logo from './components/Logo';
 import Info from './components/Info';
@@ -12,48 +15,74 @@ import Weapons from './components/Weapons';
 import Equipments from './components/Equipments';
 
 const App = () => {
-  const rightColCss =    "grid col-span-5 row-span-1 lg:col-span-3 lg:row-span-2";
-  const rowCss =         "grid grid-flow-row lg:grid-col-6 lg:grid-flow-col gap-4 col-span-6";
+  const rightColCss = "grid col-span-5 row-span-1 lg:col-span-3 lg:row-span-2";
+  const rowCss = "grid grid-flow-row lg:grid-col-6 lg:grid-flow-col gap-4 col-span-6";
   const downSectionCss = "grid grid-flow-row lg:grid-col-5 lg:grid-flow-col gap-4 p-4 w-full";
-  const upSectionCss =    downSectionCss + " lg:grid-row-3";
+  const upSectionCss = downSectionCss + " lg:grid-row-3";
+
+  const initValues = {
+    "base": 25,
+    "extras": 0,
+    "enhancement": 0,
+    "total": 25,
+    "modifier": 2
+  };
+
+  const attributes = {
+    "Strength": initValues,
+    "Dexterity": initValues,
+    "Agility": initValues,
+    "Resistence": initValues,
+    "Intelligence": initValues,
+    "Charisma": initValues,
+    "Will": initValues,
+    "Perception": initValues
+  };
+
+  const [data, setData] = useState({attributes});
 
   return (
     <div className="font-roboto min-h-screen grid bg-gray-900">
-      <Header />
-      <section>
-        <div className={upSectionCss}>
-          <div className={rowCss}>
-            <div className="col-span-1 max-w-sm"><Logo /></div>
-            <div className="col-span-5"><Info /></div>
-          </div>
+      <FormContext.Provider value={{ data, setData }}>
+        <form>
+          <Header />
+          <section>
+            <div className={upSectionCss}>
+              <div className={rowCss}>
+                <div className="col-span-1 max-w-sm"><Logo /></div>
+                <div className="col-span-5"><Info /></div>
+              </div>
 
-          <div className={rowCss}>
-              <Attributes />
-              <Stats />
-          </div>
+              <div className={rowCss}>
+                <Attributes  />
+                <Stats />
+              </div>
 
-          <div className={rightColCss}><Portrait /></div>
-        </div>
+              <div className={rightColCss}><Portrait /></div>
+            </div>
 
-        <div className="col-span-6">
-          <div className={rowCss}>
-            <Traits />
-            <Magic />
-          </div>
-        </div>
+            <div className="col-span-6">
+              <div className={rowCss}>
+                <Traits />
+                <Magic />
+              </div>
+            </div>
 
-        <div className={downSectionCss}>
-          <Skills />
-          
-          <div className="grid grid-flow-row lg:grid-col-6 gap-4 w-full">
-            <Weapons />
-            <Equipments />
-          </div>
-        </div>
+            <div className={downSectionCss}>
+              <Skills />
 
-      </section>
+              <div className="grid grid-flow-row lg:grid-col-6 gap-4 w-full">
+                <Weapons />
+                <Equipments />
+              </div>
+            </div>
 
-      <Header />
+          </section>
+
+          <Header />
+        </form>
+      </FormContext.Provider>
+
     </div >
   );
 }
--- src/components/Attributes.js
+++ src/components/Attributes.js
@@ -1,97 +1,87 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Button } from "@material-tailwind/react";
+import FormContext from "./FormContext";
 // import data from "../assets/mocks/character_sheet.json";
 
 const Attributes = () => {
-  const initValues = {
-    "base": 25,
-    "extras": 0,
-    "enhancement": 0,
-    "total": 25,
-    "modifier": 2
-  };
+  const { data, setData } = useContext(FormContext);
 
-  const attributes = {
-    "Strength": initValues,
-    "Dexterity": initValues,
-    "Agility": initValues,
-    "Resistence": initValues,
-    "Intelligence": initValues,
-    "Charisma": initValues,
-    "Will": initValues,
-    "Perception": initValues
-  };
-
-  const titlesAttributes = Object.keys(attributes);
-
-  const [formState, setFormState] = useState(attributes);
+  const titlesAttributes = Object.keys(data.attributes);
 
   const handleExtraChange = (event) => {
     if (event === null || event === 0) return;
-    console.log(formState[event.target.alt].extras, Number(event.target.value));
+    console.log(data.attributes[event.target.alt].extras, Number(event.target.value));
     const itemExtra = Number(event.target.value);
-    const itemState = formState[event.target.alt];
+    const itemState = data.attributes[event.target.alt];
     const itemTotalSum = itemState.base + itemExtra + itemState.enhancement;
     const itemModifierSum = Math.trunc(itemTotalSum / 10);
 
-    setFormState({
-      ...formState,
-      [event.target.alt]: {
-        ...formState[event.target.alt],
-        enhancement: itemState.enhancement,
-        extras: itemExtra,
-        total: itemTotalSum,
-        modifier: itemModifierSum
+    setData({
+      ...data,
+      attributes: {
+        ...data.attributes,
+        [event.target.alt]: {
+          ...data.attributes[event.target.alt],
+          enhancement: itemState.enhancement,
+          extras: itemExtra,
+          total: itemTotalSum,
+          modifier: itemModifierSum
+        }
       }
-    });
+    }
+    );
   };
 
   const handleEnhancementChange = (event) => {
     if (event === null || event === 0) return;
-    console.log(formState[event.target.alt].enhancement, Number(event.target.value));
+    console.log(data.attributes[event.target.alt].enhancement, Number(event.target.value));
     const itemEnhancement = Number(event.target.value);
-    const itemState = formState[event.target.alt];
+    const itemState = data.attributes[event.target.alt];
     const itemTotalSum = itemState.base + itemEnhancement + itemState.extras;
     const itemModifierSum = Math.trunc(itemTotalSum / 10);
 
-    setFormState({
-      ...formState,
-      [event.target.alt]: {
-        ...formState[event.target.alt],
-        enhancement: itemEnhancement,
-        extras: itemState.extras,
-        total: itemTotalSum,
-        modifier: itemModifierSum
+    setData({
+      ...data,
+      attributes: {
+        ...data.attributes,
+        [event.target.alt]: {
+          ...data.attributes[event.target.alt],
+          enhancement: itemEnhancement,
+          extras: itemState.extras,
+          total: itemTotalSum,
+          modifier: itemModifierSum
+        }
       }
-    });
+    }
+    );
   };
 
   useEffect(() => {
-    console.log(formState);
-  }, [formState])
+    console.log(data);
+  }, [data])
 
   const renderAttributes = () => titlesAttributes.map(item =>
-  (<li key={item} className="px-4">
-    <div key={item} className="inline-flex w-1/6 md:w-1/12">
+  (<li key={item + '_name'} className="px-4">
+    <div key={item + '_fullname'} className="inline-flex w-1/6 md:w-1/12">
       <span className="font-bold">{item.substring(0, 3).toUpperCase()}</span>
     </div>
-    <div key={item} className="hidden w-1/6 md:inline-flex md:w-3/12 text-center">
+    <div key={item + '_base'} className="hidden w-1/6 md:inline-flex md:w-3/12 text-center">
       <span className="hidden md:block">{item}</span>
     </div>
-    <div className="inline-flex w-1/12 text-center">{formState[item].base}</div>
+    <div className="inline-flex w-1/12 text-center">{data.attributes[item].base}</div>
     <div className="inline-flex w-1/6">
-      <input type="number" alt={item} onChange={handleExtraChange} value={formState[item].extras} className="w-full m-2 text-center" />
+      <input type="number" alt={item} onChange={handleExtraChange} value={data.attributes[item].extras} className="w-full m-2 text-center" />
     </div>
     <div className="inline-flex w-1/6">
-      <input type="number" alt={item} onChange={handleEnhancementChange} value={formState[item].enhancement} className="w-full m-2 text-center" />
+      <input type="number" alt={item} onChange={handleEnhancementChange} value={data.attributes[item].enhancement} className="w-full m-2 text-center" />
     </div>
     <div className="inline-flex w-1/6 text-center">
       <Button variant="outlined" size="sm">
-        {formState[item].total}
+        {data.attributes[item].total}
       </Button>
     </div>
-    <div className="inline-flex w-1/12 text-left">{formState[item].modifier}</div>
+    <div className="inline-flex w-1/12 text-left">{data.attributes[item].modifier}</div>
   </li>)
   );
 
