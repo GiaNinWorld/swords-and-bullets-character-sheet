4aa9454e441b486fb334fa7c564615d0eeef342b
Marcelo Meireles <macelopimeireles@gmail.com>
2023-05-01 21:02:53 +0100

    fix: console.log data when diff init state

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,16 +2,17 @@ import React, { useEffect, useContext } from "react";
 import { CONSTANTS } from "../assets/mocks/const.js";
 import { Input, Select, Option } from "@material-tailwind/react";
 import FormContext from "./FormContext";
+import { data as Initdata } from './data';
 
 function Info() {
     const { data, setData } = useContext(FormContext);
-    
+
     const titlesInfo = Object.keys(data.info);
     const titleCss = "px-3 py-2 mx-3 mb-2 font-bold text-blue-800 tracking-wider bg-blue-200 rounded-md";
 
     const capitalize = (word) => {
         return word.charAt(0).toUpperCase()
-        + word.slice(1)
+            + word.slice(1)
     }
 
     const renderOptions = (item, index) => {
@@ -25,17 +26,15 @@ function Info() {
             let values = event.split(',');
             let name = (values[0]).concat('s').toUpperCase();
             let object = CONSTANTS[name].map(item => {
-                console.log(item);
                 return item.id === Number(values[1]);
             });
-            console.log(values, object);
 
             setData({
                 ...data,
-                    info: {
-                        ...data.info,
-                        [values[0].toLowerCase()]: object
-                    }
+                info: {
+                    ...data.info,
+                    [values[0].toLowerCase()]: object
+                }
             });
         } else {
             setData({
@@ -48,16 +47,18 @@ function Info() {
         }
     };
 
-    useEffect(()=> {
-        console.log(data);
-    },[data])
+    useEffect(() => {
+        if (JSON.stringify(data) !== JSON.stringify(Initdata)) {
+            console.log(data);
+        }
+    }, [data]);
 
     const renderTitles = () => titlesInfo.map((item, index) =>
     (<li key={item} className="p-4 pt-2 pb-2">
         {(index > 1)
             ? (<Select name={item} label={capitalize(item)} onChange={handleChange} value={data.info[item]?.id}>
                 {renderOptions(item, index)}
-                </Select>
+            </Select>
             ) : <Input name={item} label={capitalize(item)} onChange={handleChange} value={data.info[item]} />
         }
     </li>)
