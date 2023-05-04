64fe7d1501100455a5f7738c5ac266419f6720ee
Marcelo Meireles <mmeireles@sporting.pt>
2023-05-04 17:22:05 +0100

    fix: life roll as build component

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { Input } from "@material-tailwind/react";
 import FormContext from "./FormContext";
 import { data as Initdata } from './data';
 
 function Stats() {
     const { data, setData } = useContext(FormContext);
+    const [lifeRoll, setLifeRoll] = useState(0);
 
     const updateStatsByAttributes = () => {
         let attributes = data.attributes;
@@ -13,16 +14,18 @@ function Stats() {
         let perceptionBonus = attributes.Perception.modifier;
         let resistenceBonus = attributes.Resistence.modifier;
         let willBonus = attributes.Will.modifier;
+        let life = (lifeRoll + resistenceBonus + willBonus);
+        console.log('lifeRoll: ',lifeRoll);
 
         stats.passiveAttention = Math.floor(perceptionBonus / 2);
         stats.naturalarmour = resistenceBonus; // + enhancements
         stats.iniciative = agilityBonus;
         stats.move = agilityBonus * .5;
-        stats.life = {current: 0, total: (Math.floor(Math.random() * 5) + 1).toString() + '+' + resistenceBonus.toString() + '+' + willBonus.toString()};
+        stats.life = {current: life, total: life};
         stats.criticalDamage = {current: 0, total: 0};
-        stats.fervor = {current: 0, total: willBonus};
-        stats.blood = {current: 0, total: resistenceBonus * 2};
-        stats.breath = {current: 0, total: resistenceBonus * 2};
+        stats.fervor = {current: willBonus, total: willBonus};
+        stats.blood = {current: resistenceBonus * 2, total: resistenceBonus * 2};
+        stats.breath = {current: resistenceBonus * 2, total: resistenceBonus * 2};
         stats.condition = [];
     
         setData({
@@ -32,10 +35,17 @@ function Stats() {
       };
       
       useEffect(() => {
-        if (JSON.stringify(data.info.race) !== JSON.stringify(Initdata.info.race)) {
+        if (JSON.stringify(data.attributes) !== JSON.stringify(Initdata.attributes)) {
           updateStatsByAttributes()
         };
-      }, [data.attributes, data.info.race])
+      }, [data.attributes])
+      
+      useEffect(() => {
+        if (lifeRoll === 0) {
+            setLifeRoll(Math.floor(Math.random() * 5) + 1);
+        };
+      }, [])
+
     return (
         <div className="rounded-md bg-blue-100 py-4 overflow-hidden h-full">
             <h1 className="px-3 py-2 mx-3 font-bold text-blue-800 tracking-wider bg-blue-200 rounded-md">Stats</h1>
@@ -44,7 +54,7 @@ function Stats() {
                 Passive Attention: {data.stats.passiveAttention }
                 </li>
                 <li className='py-2 px-4'>
-                    <Input name={'an'} label={'an'} onChange={()=>{}} value={data.stats.an} />
+                    <Input label={'Natural Armour'} onChange={()=>{}} value={data.stats.an} />
                 </li>
                 <li className='py-2 px-4'>
                     <div className='inline-flex w-1/2'>
