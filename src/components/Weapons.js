e8758f698ca83f5556668bfdbe615b892ec3982b
Marcelo Meireles <mmeireles@sporting.pt>
2023-05-04 21:37:47 +0100

    feat: base wip

--- src/components/Weapons.js
+++ src/components/Weapons.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React from "react";
 
 function Weapons() {
   return (
     <div className="rounded-md bg-blue-100 py-4 overflow-hidden">
-        <h1 className="px-3 py-2 mx-3 mb-4 font-bold text-blue-800 tracking-wider bg-blue-200 rounded-md">Weapons</h1>
+      <h1 className="px-3 py-2 mx-3 mb-4 font-bold text-blue-800 tracking-wider bg-blue-200 rounded-md">
+        Weapons
+      </h1>
+      <div className="flex flex-col">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full text-left text-sm font-light">
+                <thead className="border-b font-medium dark:border-neutral-500">
+                  <tr>
+                    <th scope="col" className="px-6 py-4">
+                      #
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      First
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Last
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Handle
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="border-b dark:border-neutral-500">
+                    <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
+                    <td className="whitespace-nowrap px-6 py-4">Mark</td>
+                    <td className="whitespace-nowrap px-6 py-4">Otto</td>
+                    <td className="whitespace-nowrap px-6 py-4">@mdo</td>
+                  </tr>
+                  <tr className="border-b dark:border-neutral-500">
+                    <td className="whitespace-nowrap px-6 py-4 font-medium">2</td>
+                    <td className="whitespace-nowrap px-6 py-4">Jacob</td>
+                    <td className="whitespace-nowrap px-6 py-4">Thornton</td>
+                    <td className="whitespace-nowrap px-6 py-4">@fat</td>
+                  </tr>
+                  <tr className="border-b dark:border-neutral-500">
+                    <td className="whitespace-nowrap px-6 py-4 font-medium">3</td>
+                    <td className="whitespace-nowrap px-6 py-4">Larry</td>
+                    <td className="whitespace-nowrap px-6 py-4">Wild</td>
+                    <td className="whitespace-nowrap px-6 py-4">@twitter</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Weapons
+export default Weapons;
