57d3a126d036b0b5cd448438d24be0f2f945d706
Marcelo Meireles <mmeireles@sporting.pt>
2023-05-05 14:43:02 +0100

    fix: new grid

--- src/App.js
+++ src/App.js
@@ -1,76 +1,80 @@
-/* eslint-disable jsx-a11y/alt-text */
 import React, { useEffect, useState } from "react";
-import { data as Initdata } from './components/data';
+import { data as Initdata } from "./components/data";
 
 import FormContext from "./components/FormContext";
-import Header from './components/Header';
-import Logo from './components/Logo';
-import Info from './components/Info';
-import Attributes from './components/Attributes';
-import Skills from './components/Skills';
-import Portrait from './components/Portrait';
-import Stats from './components/Stats';
-import Magic from './components/Magic';
-import Traits from './components/Traits';
-import Weapons from './components/Weapons';
-import Equipments from './components/Equipments';
+import Header from "./components/Header";
+import Logo from "./components/Logo";
+import Info from "./components/Info";
+import Attributes from "./components/Attributes";
+import Skills from "./components/Skills";
+import Portrait from "./components/Portrait";
+import Stats from "./components/Stats";
+import Magic from "./components/Magic";
+import Traits from "./components/Traits";
+import Weapons from "./components/Weapons";
+import Equipments from "./components/Equipments";
 
 const App = () => {
-  const rightColCss = "grid col-span-5 row-span-1 md:col-span-4 lg:col-span-3 lg:row-span-2";
-  const rowCss = "grid grid-flow-row lg:grid-col-6 lg:grid-flow-col gap-4 col-span-6";
-  const downSectionCss = "grid grid-flow-row lg:grid-col-5 lg:grid-flow-col gap-4 py-2 px-2 w-full";
+  const rightColCss =
+    "grid col-span-5 row-span-1 md:col-span-4 lg:col-span-3 lg:row-span-2";
+  const rowCss =
+    "grid grid-flow-row lg:grid-col-6 lg:grid-flow-col gap-4 col-span-6";
+  const downSectionCss =
+    "grid grid-flow-row lg:grid-col-5 lg:grid-flow-col gap-4 py-2 px-2 w-full";
   const upSectionCss = downSectionCss + " lg:grid-row-3";
 
   const [data, setData] = useState(Initdata);
 
   useEffect(() => {
     if (JSON.stringify(data) !== JSON.stringify(Initdata)) {
-        console.log(data);
+      console.log(data);
     }
-}, [data]);
+  }, [data]);
 
   return (
     <div className="font-roboto min-h-screen grid bg-gray-900">
       <FormContext.Provider value={{ data, setData }}>
-          <Header />
-          <section>
-            <div className={upSectionCss}>
-              <div className={rowCss}>
-                <div className="col-span-1 max-w-sm"><Logo /></div>
-                <div className="col-span-5"><Info /></div>
+        <Header />
+        <section>
+          <div className={upSectionCss}>
+            <div className={rowCss}>
+              <div className="col-span-1 max-w-sm">
+                <Logo />
               </div>
-
-              <div className={rowCss}>
-                <Attributes  />
-                <Stats />
-              </div>
-
-              <div className={rightColCss}><Portrait /></div>
-            </div>
-
-            <div className={downSectionCss}>
-              <div className="grid grid-flow-row grid-col-4 gap-4 w-full">
-                <Weapons />
-                <Equipments />
-              </div>
-              <div className="grid grid-flow-row grid-col-1 gap-4 w-full">
-                <Traits />
-                <Magic />
+              <div className="col-span-5">
+                <Info />
               </div>
             </div>
-            
-            <div className={downSectionCss}>
-              
-              <Skills />
 
+            <div className={rowCss}>
+              <Attributes />
+              <Stats />
             </div>
 
-          </section>
-          <Header />
+            <div className={rightColCss}>
+              <Portrait />
+            </div>
+          </div>
+
+          <div className={downSectionCss}>
+            <Skills />
+          </div>
+
+          <div className={downSectionCss}>
+            <div className="grid grid-flow-row grid-col-4 gap-4 w-full">
+              <Weapons />
+              <Equipments />
+            </div>
+            <div className="grid grid-flow-row grid-col-1 gap-4 w-full">
+              <Traits />
+              <Magic />
+            </div>
+          </div>
+        </section>
+        <Header />
       </FormContext.Provider>
-
-    </div >
+    </div>
   );
-}
+};
 
 export default App;
