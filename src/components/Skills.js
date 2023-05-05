1280ee1fa2ce06087d98b0a791ed155f5c9a76aa
Marcelo Meireles <mmeireles@sporting.pt>
2023-05-05 15:19:45 +0100

    feat: updateSkillsByAttributes wip

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,6 +12,17 @@ function Skills() {
     const inputRadioCss = "relative float-left -ml-[1.5rem] mr-1 mt-0.5 h-5 w-5 rounded-full border-2 border-solid border-blue-800 before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:border-primary checked:after:bg-primary checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,1)] focus:shadow-none focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[1] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:border-primary checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s]"
     const rollButtonCss = "my-1 px-2 py-1 text-lg font-bold text-blue-800 uppercase transition-colors duration-300 transform bg-white rounded-lg hover:bg-primary";
 
+    const updateSkillsByAttributes = () => {
+        titlesSkills.forEach((titleSkills)=>{
+            let skillsName = Object.keys(data.skills[titleSkills]);
+            skillsName.forEach((skillName) => {
+                let attributeName = data.skills[titleSkills][skillName].Attribute;
+                let levelValue = data.skills[titleSkills][skillName].level;
+                updateSkillResult(attributeName, levelValue);
+            });
+        });
+    }
+
     const updateSkillResult = (attributeName, levelValue) => {
         let total = Initdata.attributes[attributeName].total;
         console.log(attributeName, levelValue, total);
@@ -53,9 +64,7 @@ function Skills() {
 
     
     const renderSkillList = (skillList, title) => Object.keys(skillList).map(item =>
-    {
-        // console.log(Object.keys(skillList), skillList, item);
-    return (<li key={item} className="py-1 mb-2 mx-8 border-b-2 border-indigo-100">
+    (<li key={item} className="py-1 mb-2 mx-8 border-b-2 border-indigo-100">
         <div key={item} className="inline-flex w-4/12 overflow-ellipsis font-bold">{item}</div>
         <div key={item + "attribute"} className="w-1/12 text-right  hidden xl:inline-flex">{skillList[item].Attribute.substring(0, 3).toUpperCase()}</div>
         <div className="inline-flex w-5/12 justify-around">
@@ -70,13 +79,11 @@ function Skills() {
                 { skillList[item].Result.length < 2 ? "0" + skillList[item].Result: skillList[item].Result}
             </a>
         </div>
-    </li>)}
+    </li>)
     );
 
     const renderSkills = () => titlesSkills.map(item =>
-       { 
-        // console.log(data.skills[item], item);
-    return (<li key={item}>
+    (<li key={item}>
         <div className="rounded-md bg-blue-100 overflow-hidden">
             <h1 className="px-3 py-2 mx-3 mb-2 font-bold text-blue-400 tracking-wider bg-blue-200 rounded-md">{item}</h1>
             <ul className="mx-4 p-0 pb-2 mb-2 text-blue-600 border-b-2 border-indigo-100">
@@ -95,8 +102,21 @@ function Skills() {
                 {data && renderSkillList(data.skills[item], item)}
             </ul>
         </div>
-    </li>)}
+    </li>)
     );
+
+    useEffect(() => {
+        if (JSON.stringify(data.attributes) !== JSON.stringify(Initdata.attributes)) {
+          updateSkillsByAttributes()
+        };
+     }, [data.attributes])
+    
+     useEffect(() => {
+        if (JSON.stringify(data.info.race) !== JSON.stringify(Initdata.info.race)) {
+          updateSkillsByAttributes()
+        };
+     }, [data.info.race])
+    
     return (
         <div className="rounded-md bg-blue-100 py-4 overflow-hidden">
             <h1 className="px-3 py-2 mx-3 mb-4 font-bold text-blue-800 tracking-wider bg-blue-200 rounded-md">Skills</h1>
